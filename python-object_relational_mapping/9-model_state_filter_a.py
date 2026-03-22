68de0d08a22579b67a174df221bcc705e1efb938
izzooooooo <izzetqasimov2007@gmail.com>
2026-03-22 19:29:58 +0000

    ??

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,10 +14,17 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
+    # Session yaradılır
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # Query: Adında 'a' hərfi olanları id üzrə artan sıra ilə götürürük
     states = session.query(State).filter(State.name.like('%a%')) \
                                  .order_by(State.id).all()
+
+    # Nəticələri çap edirik
     for state in states:
         print("{}: {}".format(state.id, state.name))
+
+    # Session bağlanır
     session.close()
